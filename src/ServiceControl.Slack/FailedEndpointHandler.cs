74bcfcbcab3036adc8ca5f2d05e93da9d3225201
andreasohlund <andreas.ohlund@particular.net>
2015-06-20 15:37:40 +0200

    Add handler for failed heartbeats

---
+++ src/ServiceControl.Slack/FailedEndpointHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceControl.Slack
+{
+    using NServiceBus;
+    using ServiceControl.Contracts;
+
+    public class FailedEndpointHandler:IHandleMessages<HeartbeatStopped>
+    {
+        public void Handle(HeartbeatStopped message)
+        {
+            
+        }
+    }
+}
