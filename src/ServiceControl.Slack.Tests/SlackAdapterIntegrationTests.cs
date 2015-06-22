2e23869bc0279cfa68a61d4ea669bc8ab681f57d
andreasohlund <andreas.ohlund@particular.net>
2015-06-22 08:22:46 +0200

    Adding a test for sends

---
+++ src/ServiceControl.Slack.Tests/SlackAdapterIntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿namespace ServiceControl.Slack.Tests
+{
+    using System;
+    using NUnit.Framework;
+    using ServiceControl.Slack.Api;
+
+    public class SlackAdapterIntegrationTests
+    {
+        [Test]
+        public async void PostMessage()
+        {
+            var token = Environment.GetEnvironmentVariable("ServiceControl.Slack.Token",EnvironmentVariableTarget.User);
+            if (token == null)
+            {
+                throw new Exception("Couldn't find an slack api token, please add a user env variable named `ServiceControl.Slack.Token`");
+            }
+            var api = new SlackAdapter(token);
+
+            await api.Start();
+
+            await api.Send("sc-bot-test", "Hello from integration tests");
+
+            Console.Out.WriteLine("All seems good, please check: https://particular-test.slack.com/messages/sc-bot-test/");
+        }
+    }
+}
