208ec989201a1b668fae0c0bd8130fc551e7804e
Mark Gould <markg@spindlemedia.com>
2015-12-03 23:04:39 -0600

    Removed Api code and several NuGet packages. Switched to using Slack.Webhooks for a simplified experience.
    See https://api.slack.com/incoming-webhooks for details

--- src/ServiceControl.Slack.Tests/SlackAdapterIntegrationTests.cs
+++ src/ServiceControl.Slack.Tests/SlackAdapterIntegrationTests.cs
@@ -2,13 +2,12 @@
 {
     using System;
     using NUnit.Framework;
-    using ServiceControl.Slack.Api;
 
     public class SlackAdapterIntegrationTests
     {
         [Test]
         [Explicit]
-        public async void PostMessage()
+        public void PostMessage()
         {
             var token = Environment.GetEnvironmentVariable("ServiceControl.Slack.Token", EnvironmentVariableTarget.User);
 
@@ -17,11 +16,9 @@
                 throw new Exception("Couldn't find an slack api token, please add a user env variable named `ServiceControl.Slack.Token`");
             }
 
-            var api = new SlackAdapter(token);
+            var api = new SlackNotifier(token);
 
-            await api.Start();
-
-            await api.Send("sc-bot-test", "Hello from integration tests");
+            api.Notify("Hello from integration tests");
 
             Console.Out.WriteLine("All seems good, please check: https://particular-test.slack.com/messages/sc-bot-test/");
         }
